ad8fa6293a5a5afde3d9d1047a6949881cc14d21
Emina Jordamovic <ema.jordamovic@gmail.com>
2020-03-03 11:17:33 +0100

    Wir geben nun mehrere Zeilen aus

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld world");
+        //TODO: new method for HelloWorld
+        System.out.println("HelloWorld Campus");
+        System.out.println("Hellocampus");
+
+    }
+}
